49286e74c3b27381a114b469f6f2333b6b9143b3
xxglb016 <pengbaiping@gmail.com>
2022-11-30 16:37:15 +0800

    配置中心和网关,视频第28集已结束

---
+++ pbpmall-gateway/src/test/java/com/pbpmall/pbpmallgateway/PbpmallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbpmall.pbpmallgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbpmallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
